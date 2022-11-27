f3ed58a28c5005f9affae92bf804e0eadada408c
pontusolsson03 <58231866+pontusolsson03@users.noreply.github.com>
2022-11-27 22:26:06 +0100

    Code

---
+++ script.js
@@ -0,0 +1,93 @@
+//? Vad är JavaScript? Bakgrund:
+
+//* - Den "tredje pelaren" i webbutveckling, används för användarinteraktion
+//* - Ingen tråkig historia, det är inte så viktigt för tilfället
+//* - Tillåter funktionalitet så som knappar, formulär - allt som inte är statiskt
+//* - Kan köras både client-side och server-side, men främst client
+//* - Demonstration av exempel; knapp
+
+// let button = document.getElementById("button");
+
+// button.addEventListener("click", function () {
+//   alert("Herregud, du klickade!");
+//   console.log("Herregud, du klickade! (Men nu är jag i konsolen hihi)");
+// });
+
+//? Lite syntax:
+
+//* - Variabler, vad är det?
+//* - Användning av variabler
+//* - Deklarering - let, const, var
+
+// let firstVariable = 1;
+// const secondVariable = 2;
+// var thirdVariable = 3;
+
+//* Försöka ändra värde
+// ...Variable = 4;
+
+//? Aritmetik (matte):
+
+// let firstVar = 8;
+// let secondVar = 4;
+// let thirdVar = firstVar + 1;
+// thirdVar = firstVar + secondVar
+// thirdVar = firstVar * secondVar
+// thirdVar = firstVar / secondVar
+// thirdVar = firstVar++
+
+//*     + 	Addition
+//*     - 	Subtraction
+//*     * 	Multiplication
+//*     ** 	Exponentiation (ES2016)
+//*     / 	Division
+//*     % 	Modulus (Remainder)
+//*     ++ 	Increment
+//*     -- 	Decrement
+
+//? Data types:
+
+//*      String     ("Hejsan!", "420")
+//*      Number     (1337)
+//*      Boolean    (True/False)
+//*      Undefined
+//*      Null
+
+//? Kolla värde:
+
+//* Hur/när används det?
+
+//*      ==
+//*      ===    (= = =)
+//*      <=     (< =)
+//*      ==>    (= >)
+//*      <=     (< = =)
+//*      ==>    (= = >)
+//*      !=     (! =)
+//*      !==    (! = =)
+
+//? Manipulationg the DOM (Document Object Model):
+
+//* - Vad är DOM? (Mindre viktigt)
+//* - Hur använder/manipulerar/ändrar vi den? + introduktion till funktioner:
+
+// let button = document.getElementById("button");
+// let myText = document.getElementById("text");
+
+// button.addEventListener("click", function () {
+//   myText.style.display = "none";
+// });
+
+// button.addEventListener("click", function () {
+//   if (myText.style.display == "none") {
+//     myText.style.display = "block";
+//   } else {
+//     myText.style.display = "none";
+//   }
+// });
+
+//! Exempel på egna projekt i JavaScript
+
+//! Testa själva
+
+//* Gör nytt projekt tillsammans också testa tillsammans
